dce72dfa76e43efcd4280902538a54f78cfd7709
Karol Gruszczyński <lapera.carlos@gmail.com>
2021-04-30 17:25:22 +0200

    Added class Cart (entity carts) without relations between other entities & CartDao interface as a repository controller

---
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -0,0 +1,21 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Data
+@Entity(name = "carts")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_id")
+    private Long cartId;
+
+    @NotNull
+    @Column(name = "order_date")
+    private Date orderDate;
+}
