1fd527539bc1d15e0659396c93f3d180d87cdfd8
anna850412 <73561196+anna850412@users.noreply.github.com>
2021-05-03 19:06:53 +0200

    Added interface ProductDao extends CrudRepository

---
+++ src/main/java/com/kodilla/ecommercee/dao/ProductDao.java
@@ -0,0 +1,9 @@
+package com.kodilla.ecommercee.dao;
+
+import com.kodilla.ecommercee.domain.Product;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductDao extends CrudRepository <Product, Long> {
+}
