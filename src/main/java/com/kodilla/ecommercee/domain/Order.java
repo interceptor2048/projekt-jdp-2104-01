8b86869a8a2f3ff569ded1de9511980ad84bfdcf
laperacarlos <75305194+laperacarlos@users.noreply.github.com>
2021-05-06 16:23:52 +0200

    Updated names of reference columns in @ManyToMany relation to be in line with scheme

--- src/main/java/com/kodilla/ecommercee/domain/Order.java
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -32,8 +32,8 @@ public class Order {
 
     @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(name = "JOIN_PRODUCT_ORDER",
-            joinColumns = {@JoinColumn(name = "ORDER_ID", referencedColumnName = "ORDER_ID")},
-            inverseJoinColumns = {@JoinColumn(name = "PRODUCT_ID", referencedColumnName = "PRODUCT_ID")}
+            joinColumns = {@JoinColumn(name = "ORDER_ID", referencedColumnName = "ID")},
+            inverseJoinColumns = {@JoinColumn(name = "PRODUCT_ID", referencedColumnName = "ID")}
     )
     private List<Product> productList;
 }
