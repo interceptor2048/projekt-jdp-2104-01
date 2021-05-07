58b7918289b60df53bfec01f6b9412f9082ea058
Oskar Anczyk <myamy45.5@gmail.com>
2021-05-07 16:51:43 +0200

    Resolved confilicts

--- src/main/java/com/kodilla/ecommercee/controller/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/controller/GroupController.java
@@ -1,10 +1,10 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.dao.GroupDao;
 import com.kodilla.ecommercee.domain.GroupDto;
 import com.kodilla.ecommercee.domain.GroupNotFoundException;
 import com.kodilla.ecommercee.domain.ProductsGroup;
 import com.kodilla.ecommercee.mapper.GroupMapper;
-import com.kodilla.ecommercee.service.DbService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -18,37 +18,38 @@ import java.util.List;
 @RequiredArgsConstructor
 public class GroupController {
 
-    private final DbService service;
+
+    private final GroupDao service;
     private final GroupMapper groupMapper;
 
     @RequestMapping(method = RequestMethod.GET, value = "getGroups")
     public List<GroupDto> getGroups() {
-        List<ProductsGroup> groups = service.getAllGroups();
+        List<ProductsGroup> groups = service.findAll();
         return groupMapper.mapToGroupDtoList(groups);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getGroup")
     public GroupDto getGroup(@RequestParam Long groupId) throws GroupNotFoundException {
         return groupMapper.mapToGroupDto(
-                service.getGroup(groupId).orElseThrow(GroupNotFoundException::new)
+                service.findById(groupId).orElseThrow(GroupNotFoundException::new)
         );
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "createGroup", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void createGroup(@RequestBody GroupDto groupDto) {
         ProductsGroup group = groupMapper.mapToGroup(groupDto);
-        service.saveGroup(group);
+        service.save(group);
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "updateGroup")
     public GroupDto updateGroup(@RequestBody GroupDto groupDto) {
         ProductsGroup group = groupMapper.mapToGroup(groupDto);
-        ProductsGroup savedGroup = service.saveGroup(group);
+        ProductsGroup savedGroup = service.save(group);
         return groupMapper.mapToGroupDto(savedGroup);
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteGroup")
     public void deleteGroup(Long groupId) {
-        service.deleteGroup(groupId);
+        service.deleteById(groupId);
     }
 }
--- src/main/java/com/kodilla/ecommercee/dao/GroupDao.java
+++ src/main/java/com/kodilla/ecommercee/dao/GroupDao.java
@@ -4,6 +4,21 @@ import com.kodilla.ecommercee.domain.ProductsGroup;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface GroupDao extends CrudRepository<ProductsGroup, Long> {
+
+    @Override
+    List<ProductsGroup> findAll();
+
+    @Override
+    ProductsGroup save(ProductsGroup group);
+
+    @Override
+    Optional<ProductsGroup> findById(Long groupId);
+
+    @Override
+    void deleteById(Long groupId);
 }
