8b784f48e8103d0750c389bfc1e053ba2f94eeed
WojtekSJ <wojnad@o2.pl>
2021-05-11 21:57:40 +0200

    Zmiana definicji relacji miedzy encjami User Cart

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -11,6 +11,54 @@ import java.util.List;
 @Data
 @Entity(name = "CARTS")
 public class Cart {
+    private Long cartId;
+    private Date orderDate;
+    private List<Product> listOfProducts  = new ArrayList<>();
+    private User user;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
+    @Column(name = "CART_ID")
+    public Long getCartId() {
+        return cartId;
+    }
+    @NotNull
+    @Column(name = "ORDER_DATE")
+    public Date getOrderDate() {
+        return orderDate;
+    }
+    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "cartList")
+    public List<Product> getListOfProducts() {
+        return listOfProducts;
+    }
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "USER_ID")
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+
+
+    private void setCartId(Long cartId) {
+        this.cartId = cartId;
+    }
+
+    private void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    private void setListOfProducts(List<Product> listOfProducts) {
+        this.listOfProducts = listOfProducts;
+    }
+}
+
+/*@Data
+@Entity(name = "CARTS")
+public class Cart {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,4 +74,4 @@ public class Cart {
     private List<Product> listOfProducts  = new ArrayList<>();
 
 
-}
+}*/
--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -31,6 +31,106 @@ public class User {
     }
 
     public User() {}
+    private Long id;
+    private String username;
+    private int status;
+    private String userKey;
+    private LocalDateTime expirationTime;
+    private List<Order> listOfOrders = new ArrayList<>();
+    private Cart cart;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+    @NotNull
+    @Column(name = "USERNAME", unique = true)
+    public String getUsername() {
+        return username;
+    }
+
+    @NotNull
+    @Column(name = "STATUS")
+    public int getStatus() {
+        return status;
+    }
+
+    @NotNull
+    @Column(name = "USER_KEY", unique = true)
+    public String getUserKey() {
+        return userKey;
+    }
+
+    @Column(name = "EXPIRATION_TIME")
+    public LocalDateTime getExpirationTime() {
+        return expirationTime;
+    }
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "CART_ID")
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    @OneToMany(
+            targetEntity = Order.class,
+            mappedBy = "user",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    public List<Order> getListOfOrders() {
+        return listOfOrders;
+    }
+
+    private void setId(Long id) {
+        this.id = id;
+    }
+
+    private void setUsername(String username) {
+        this.username = username;
+    }
+
+    private void setStatus(int status) {
+        this.status = status;
+    }
+
+    private void setUserKey(String userKey) {
+        this.userKey = userKey;
+    }
+
+    private void setExpirationTime(LocalDateTime expirationTime) {
+        this.expirationTime = expirationTime;
+    }
+
+    private void setListOfOrders(List<Order> listOfOrders) {
+        this.listOfOrders = listOfOrders;
+    }
+}
+/*@Data
+@Entity(name = "USERS")
+public class User {
+    public User(Long id, @NotNull String username, @NotNull int status, @NotNull String userKey, LocalDateTime expirationTime) {
+        this.id = id;
+        this.username = username;
+        this.status = status;
+        this.userKey = userKey;
+        this.expirationTime = expirationTime;
+    }
+
+    public User(Long id, String username, int status, String userKey, LocalDateTime expirationTime, List<Order> listOfOrders) {
+        this.id = id;
+        this.username = username;
+        this.status = status;
+        this.userKey = userKey;
+        this.expirationTime = expirationTime;
+        this.listOfOrders = listOfOrders;
+    }
+
+    public User() {}
 
 
     @Id
@@ -65,4 +165,4 @@ public class User {
     public void setListOfOrders(List<Order> listOfOrders) {
         this.listOfOrders = listOfOrders;
     }
-}
+}*/
