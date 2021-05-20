aea23f6c5c8ac576335576c47b742ccf1822e79a
Oskar Anczyk <myamy45.5@gmail.com>
2021-05-20 18:53:10 +0200

    Corrected OrderController, added OrderDbService

--- src/main/java/com/kodilla/ecommercee/controller/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/controller/OrderController.java
@@ -3,10 +3,11 @@ package com.kodilla.ecommercee.controller;
 
 import com.kodilla.ecommercee.controller.exception.OrderNotFoundException;
 import com.kodilla.ecommercee.dao.OrderDao;
-import com.kodilla.ecommercee.domain.Order;
-import com.kodilla.ecommercee.domain.OrderDto;
-import com.kodilla.ecommercee.domain.User;
+import com.kodilla.ecommercee.domain.*;
+import com.kodilla.ecommercee.exception.ProductsGroupNotFoundException;
+import com.kodilla.ecommercee.exception.UserNotFoundException;
 import com.kodilla.ecommercee.mapper.OrderMapper;
+import com.kodilla.ecommercee.service.OrderDbService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -20,40 +21,39 @@ import java.util.List;
 @RequiredArgsConstructor
 public class OrderController {
 
-    private final OrderDao service;
+    private final OrderDbService service;
     private final OrderMapper mapper;
 
     @RequestMapping(method = RequestMethod.GET,value = "getOrders")
     public List<OrderDto> getOrders(){
-        List<Order> foundList = service.findAll();
+        List<Order> foundList = service.findAllOrders();
         List<OrderDto> returnList = mapper.mapToOrderDtoList(foundList);
         return returnList;
     }
 
     @RequestMapping(method = RequestMethod.POST,value = "createOrder", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void createOrder(@RequestBody OrderDto orderDto){
-        orderDto.setOrderDate(LocalDateTime.now());
-        Order order = mapper.mapToOrder(orderDto);
-        service.save(order);
+    public void createOrder(@RequestParam Long userId, @RequestBody List<ProductDto> productList) throws UserNotFoundException, ProductsGroupNotFoundException {
+        Order order = new Order(LocalDateTime.now());
+
+        service.createOrder(order, userId, productList);
     }
 
     @RequestMapping(method = RequestMethod.GET,value = "getOrder")
     public OrderDto getOrder(@RequestParam Long orderId) throws OrderNotFoundException {
-        return mapper.mapToOrderDto(service.findById(orderId)
+        return mapper.mapToOrderDto(service.findOrderById(orderId)
                 .orElseThrow(OrderNotFoundException::new));
     }
 
     @RequestMapping(method = RequestMethod.PUT,value = "updateOrder")
-    public OrderDto updateOrder(@RequestBody OrderDto orderDto){
-        Order order = mapper.mapToOrder(orderDto);
-        Order savedOrder = service.save(order);
-        return mapper.mapToOrderDto(savedOrder);
+    public OrderDto updateOrder(@RequestParam Long orderId, @RequestBody List<ProductDto> productList) throws OrderNotFoundException, ProductsGroupNotFoundException{
+        Order order = service.findOrderById(orderId).orElseThrow(OrderNotFoundException::new);
+        return service.updateOrder(order,productList);
     }
 
 
     @RequestMapping(method = RequestMethod.DELETE,value = "deleteOrder")
     public void deleteOrder(@RequestParam Long orderId){
-        service.deleteById(orderId);
+        service.deleteOrderById(orderId);
     }
 
 }
