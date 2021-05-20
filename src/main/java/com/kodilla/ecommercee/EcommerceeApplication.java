1257f13d3b14824dc87dad601a39a1c5ad8d15c3
TomekSzaty <75204970+TomekSzaty@users.noreply.github.com>
2021-05-20 19:11:17 +0200

    usunięcie zbędnych zalezności -ogólny refactor

--- src/main/java/com/kodilla/ecommercee/EcommerceeApplication.java
+++ src/main/java/com/kodilla/ecommercee/EcommerceeApplication.java
@@ -2,10 +2,8 @@ package com.kodilla.ecommercee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class EcommerceeApplication {
 
     public static void main(String[] args) {
